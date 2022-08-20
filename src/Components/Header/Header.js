0ad15626b7d5ac329a6dafd16007c83d90c8bb54
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-08-20 11:50:26 +0600

    Home page banner section responsive design complete.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,29 @@
 import React from "react";
+// import bannerImg from "../../assets/header_img.jpg";
+// import bannerImg from '../../assets/learning.jpg'
+import bannerImg from '../../assets/e-learning-removebg-preview.png'
 import "./Header.scss";
 
 const Header = () => {
   return (
     <div className="header__container">
       <div className="header__content">
-        <h2>Header content</h2>
+        <h4>
+          Start your favourite courses for <span>Free</span>
+        </h4>
+        <h2>
+          Now learning from <span>iShkool</span>
+          <br />
+          And build your bright career.
+        </h2>
+        <p>
+          Start learning anything you want. Be the best. <br /> Make the world a
+          better place!
+        </p>
+        <button>Start A Course</button>
       </div>
       <div className="header__img">
-        <h2>Header img</h2>
+        <img src={bannerImg} alt="img" />
       </div>
     </div>
   );
