4db14f45163eccac5c4ef49cf59c66c1ee42938d
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-08-20 19:47:40 +0600

    Join as a instructor section complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./Components/NavBar/NavBar";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Courses from "./Components/Courses/Courses";
+import JoinInstructor from "./Components/JoinInstructor/JoinInstructor";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavBar />
       <Header/>
       <Courses/>
+      <JoinInstructor/>
     </div>
   );
 }
